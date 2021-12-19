4acb536a8065aa16d40fdbb981a3c5b010384591
LukaszMalinowski <lukasz.malinowski10@gmail.com>
2021-12-19 18:17:48 +0100

    Added error message in TeamsPage

--- src/pages/TeamsPage.js
+++ src/pages/TeamsPage.js
@@ -1,6 +1,6 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {deleteTeam, fetchAllTeams, postTeam} from "../services/team-service";
-import {Button, Card} from "@mui/material";
+import {Alert, Button, Card, Snackbar} from "@mui/material";
 import TeamCard from "../components/Team/TeamCard";
 import NewTeamDialog from "../components/Team/NewTeamDialog";
 import {AuthContext} from "../contexts/AuthContext";
@@ -10,6 +10,11 @@ const TeamsPage = () => {
     const [teams, setTeams] = useState([]);
     const [open, setOpen] = useState(false);
     const {token} = useContext(AuthContext);
+    const [snackbar, setSnackbar] = useState({
+        open: false,
+        type: "info",
+        message: ""
+    });
 
     useEffect(() => {
         fetchAllTeams()
@@ -23,11 +28,24 @@ const TeamsPage = () => {
     const addNewTeam = name => {
         postTeam({name: name}, token)
             .then(team => setTeams([...teams, team]))
+            .catch(() => setSnackbar({
+                open: true,
+                type: "error",
+                message: "You need to be logged in to do this"
+            }));
     }
 
     const handleDeleteTeam = (teamId) => {
         deleteTeam(teamId, token)
             .then(() => setTeams(teams.filter(team => team.teamId !== teamId)))
+    }
+
+    const handleSnackbarClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setSnackbar({...snackbar, open: false});
     }
 
     const teamsMapped = teams.map(team => <TeamCard {...team} key={team.teamId} deleteTeam={handleDeleteTeam}/>);
@@ -40,6 +58,11 @@ const TeamsPage = () => {
                     <Button sx={{width: "100%"}} onClick={toggleDialog}>Add new team</Button>
                 </Card>
             </div>
+            <Snackbar open={snackbar.open} autoHideDuration={3000} onClose={handleSnackbarClose}>
+                <Alert onClose={handleSnackbarClose} severity={snackbar.type} sx={{width: '100%'}}>
+                    {snackbar.message}
+                </Alert>
+            </Snackbar>
             <NewTeamDialog open={open} toggleDialog={toggleDialog} addNewTeam={addNewTeam}/>
         </div>
     );
